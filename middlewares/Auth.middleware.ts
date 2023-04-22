a09f3213c2701560e760695f7526eda15f864774
Zhao Chen <zhao1995.chen@gmail.com>
2023-04-22 23:08:07 +0800

    feat: add isAuth()

--- middlewares/Auth.middleware.ts
+++ middlewares/Auth.middleware.ts
@@ -1,5 +1,9 @@
 import jwt from 'jsonwebtoken'
 import { ILogin } from '../interfaces/Login.interface'
+import { Request, Response } from 'express'
+import { errorHandler } from '../services/errorHandler'
+import { User } from '../models/User.model'
+import { Error } from 'mongoose'
 
 // 產生 token
 const generateToken = (user: ILogin) => {
@@ -10,6 +14,33 @@ const generateToken = (user: ILogin) => {
   )
 }
 
+// 驗證 token
+const isAuth = async (req: Request, res: Response) => {
+  try {
+    let token: string, _id: string
+
+    // 確認 token 存在及格式正確
+    if (req.headers.authorization?.startsWith('Bearer')) token = req.headers.authorization.split(' ')[1]
+    if (!token) throw '請登入後操作'
+
+    // 驗證 token 值正確
+    jwt.verify(token, process.env.JWT_SECRET, (e: Error, payload: ILogin) => {
+      if (e) {
+        throw e
+      }
+      _id = payload._id
+    })
+
+    const currentUser = await User.findById(_id)
+
+    // 傳遞會員資料給後續操作
+    req.body.user = currentUser 
+  } catch(e) {
+    errorHandler(res, e)
+  }
+}
+
 export {
-  generateToken
+  generateToken,
+  isAuth
 }
