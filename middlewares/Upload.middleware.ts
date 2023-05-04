f353f8e73905efb37f42c57f5b0a0153e4221af7
Zhao Chen <zhao1995.chen@gmail.com>
2023-05-04 22:00:14 +0800

    refactor: adjust data format

--- middlewares/Upload.middleware.ts
+++ middlewares/Upload.middleware.ts
@@ -1,6 +1,7 @@
 import { Request } from 'express'
 import multer, { FileFilterCallback } from 'multer'
 import { File } from '../interfaces/Upload.interface'
+import { ERROR } from '../const'
 
 
 const upload = multer({
@@ -9,7 +10,7 @@ const upload = multer({
   },
   fileFilter(req: Request, file: File, cb: FileFilterCallback) {
     // 非圖檔
-    if (!file.originalname.match(/\.(jpe?g|png|svg|gif)$/)) cb(new Error('請上傳正確的檔案格式'))
+    if (!file.originalname.match(/\.(jpe?g|png|svg|gif)$/)) cb(new Error(ERROR.ERROR_FORMAT))
     // 圖片
     cb(null, true)
   }
